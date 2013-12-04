cad5632a4392486fa909c248400a1c74f891e5de
e <e@epc.(none)>
2013-12-04 17:52:00 +0200

    finished, animation working, will try next to use TweenMax.fromTo instead of TweenMax.set and TweenMax.to

--- public_html/scripts/app.js
+++ public_html/scripts/app.js
@@ -5,9 +5,9 @@ app.controller('AppCtrl', function($scope) {
     $scope.currPage = 0;
     
     $scope.gotoPage = function(pageIndex, direction) {
-        direction = direction || 'Left';
-        $scope.isSlideLeft = direction === 'Left';
-        $scope.isSlideRight = direction === 'Right';
+        $scope.direction = direction || 'left';
+        $scope.isSlideLeft = $scope.direction === 'left';
+        $scope.isSlideRight = $scope.direction === 'right';
         
         if(pageIndex >= numPages) {
             pageIndex = 0;
@@ -19,3 +19,48 @@ app.controller('AppCtrl', function($scope) {
     };
 });
 
+app.animation('.slide-animation', function() {
+    return {
+        beforeAddClass: function(element, className, done) {
+            var scope = element.scope();
+
+            if(className == 'ng-hide') {
+                var endPoint = '100%';
+                if(scope.direction !== 'right') endPoint = '-100%';
+
+                TweenMax.to(element, 0.5, { left: endPoint, onComplete: done });
+            } else {
+                done();
+            }
+        },
+        addClass: function (element, className, done) {
+            // you can achieve the same thing in 'addClass' as with the code from 'beforeAddClass' by removing the ng-hide class before the animation start and adding it back as soon as the animation as finished, see below:
+            /*var scope = element.scope();
+
+            if(className == 'ng-hide') {
+                var endPoint = '100%';
+                if(scope.direction !== 'right') endPoint = '-100%';
+                element.removeClass('ng-hide');
+                TweenMax.to(element, 0.5, { left: endPoint, onComplete: function() {
+                    this.target.addClass('ng-hide');
+                    done();
+                }});
+            } else {
+                done();
+            }*/
+        },
+        removeClass: function (element, className, done) {
+            var scope = element.scope();
+
+            if(className == 'ng-hide') {
+                element.removeClass('ng-hide');
+                var startPoint = '100%';
+                if(scope.direction === 'right') startPoint = '-100%';
+                TweenMax.set(element, { left: startPoint });
+                TweenMax.to(element, 0.5, {left: 0, onComplete: done});
+            } else {
+                done();
+            }
+        }
+    };
+});
