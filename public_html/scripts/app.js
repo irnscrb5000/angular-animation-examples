ffd27dd76fd0dbe851f0ebb7d53b7b0f38466440
e <e@epc.(none)>
2013-12-05 10:50:25 +0200

    still buggy

--- public_html/scripts/app.js
+++ public_html/scripts/app.js
@@ -21,44 +21,25 @@ app.controller('AppCtrl', function($scope) {
 
 app.animation('.slide-animation', function() {
     return {
-        beforeAddClass: function(element, className, done) {
+        leave: function(element, className, done) {
+            console.log('leave fired');
             var scope = element.scope();
 
-            if(className == 'ng-hide') {
+            if(className == 'ng-leave') {
                 var endPoint = '100%';
-                //var endPoint = element[0].offsetWidth;
-                if(scope.direction !== 'right') endPoint = '-100%' /*endPoint = -endPoint*/;
-
+                if(scope.direction !== 'right') endPoint = '-100%';
                 TweenMax.to(element, 0.5, { left: endPoint, onComplete: done });
             } else {
                 done();
             }
         },
-        addClass: function (element, className, done) {
-            // you can achieve the same thing in 'addClass' as with the code from 'beforeAddClass' by removing the ng-hide class before the animation start and adding it back as soon as the animation as finished, see below:
-            /*var scope = element.scope();
-
-            if(className == 'ng-hide') {
-                var endPoint = '100%';
-                if(scope.direction !== 'right') endPoint = '-100%';
-                element.removeClass('ng-hide');
-                TweenMax.to(element, 0.5, { left: endPoint, onComplete: function() {
-                    this.target.addClass('ng-hide');
-                    done();
-                }});
-            } else {
-                done();
-            }*/
-        },
-        removeClass: function (element, className, done) {
+        enter: function (element, className, done) {
+            console.log('enter fired');
             var scope = element.scope();
 
-            if(className == 'ng-hide') {
-                element.removeClass('ng-hide');
+            if(className == 'ng-enter') {
                 var startPoint = '100%';
                 if(scope.direction === 'right') startPoint = '-100%';
-                //TweenMax.set(element, { left: startPoint });
-                //TweenMax.to(element, 0.5, {left: 0, onComplete: done});
                 TweenMax.fromTo(element, 0.5, {left: startPoint}, {left: 0, onComplete: done});
             } else {
                 done();
