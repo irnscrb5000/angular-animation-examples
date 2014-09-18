4c392b4518f21b4ac52cb849e930dce986c0ffa6
magicgang <magicgang@magicgang>
2014-09-18 12:22:18 +0300

    removed trailing whitespace; fixed a missing end tag in index.html

--- public_html/scripts/app.js
+++ public_html/scripts/app.js
@@ -3,12 +3,12 @@ var app = angular.module('app', ['ngAnimate']);
 app.controller('AppCtrl', function($scope, $timeout) {
     var numPages = 3;
     $scope.currPage = 0;
-    
+
     $scope.gotoPage = function(pageIndex, direction) {
         direction = direction || 'left';
         $scope.isSlideLeft = direction === 'left';
         $scope.isSlideRight = direction === 'right';
-        
+
         if(pageIndex >= numPages) {
             pageIndex = 0;
         } else if(pageIndex < 0) {
