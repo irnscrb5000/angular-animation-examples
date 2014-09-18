d8ae6361854b0361f5b77846db2efb0b81b3f2ec
magicgang <magicgang@magicgang>
2014-09-18 12:24:54 +0300

    removed trailing whitespace

--- public_html/scripts/app.js
+++ public_html/scripts/app.js
@@ -3,22 +3,17 @@ var app = angular.module('app', ['ngAnimate']);
 app.controller('AppCtrl', function($scope, $timeout) {
     var numPages = 3;
     $scope.currPage = 0;
-    
+
     $scope.gotoPage = function(pageIndex, direction) {
         direction = direction || 'left';
         $scope.isSlideLeft = direction === 'left';
         $scope.isSlideRight = direction === 'right';
-        
+
         if(pageIndex >= numPages) {
             pageIndex = 0;
         } else if(pageIndex < 0) {
             pageIndex = numPages - 1;
         }
-
-        /*var selEl = document.querySelector('.my-switch-container').children[pageIndex];
-        selEl.addEventListener('transitionend', function(e) {
-            debugger;
-        });*/
 
         $timeout(function() {
             $scope.currPage = pageIndex;
